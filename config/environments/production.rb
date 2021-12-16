e7266321b195d42dc40046e98dcf6d1660c4eb9e
Alexander <sanyaalexander1901@gmail.com>
2021-12-16 15:10:58 +0200

    heroku final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => "smtp.gmail.com" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
